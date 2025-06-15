c5ba0558da5726169ef1a05e7a7833828be68152
Ahmad Raza <ahmadrazamul09@gmail.com>
2025-06-15 05:34:36 +0500

    first commit

---
+++ src/main/java/com/student/management/HibernateStudentCourseApplication.java
@@ -0,0 +1,13 @@
+package com.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateStudentCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateStudentCourseApplication.class, args);
+	}
+
+}
